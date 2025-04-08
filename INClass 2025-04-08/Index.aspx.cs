e0149a90834ac625e58cefd8b356255a13bfa982
Alex Jarvis <awjarvis2004@gmail.com>
2025-04-08 12:21:59 -0400

    Added total.

--- INClass 2025-04-08/Index.aspx.cs
+++ INClass 2025-04-08/Index.aspx.cs
@@ -11,7 +11,27 @@ namespace INClass_2025_04_08
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            cmdOK.Click += button1_Click; //hooks the event to our code
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cmdOK.Click += button1_Click; //hooks the event to our code
 
+            DateTime startDate = CalCheckInDate.SelectedDate;
+            DateTime endDate = CalCheckOutDate.SelectedDate;
+
+            TimeSpan difference = endDate - startDate;
+            double days = difference.TotalDays;
+
+            double multiplier = 200;
+            double result = multiplier * days;
+            double GrandTotal = result + (result * 0.15);
+
+            lblResult.Text += GrandTotal.ToString();
+            // 1. compute the # of days between the two calendar controls
+            // 2. multiply that # by $200 (per day)
+            // 3. apply 15% sales tax to that value
+            // 4. display the total with tax in our label
         }
     }
 }
